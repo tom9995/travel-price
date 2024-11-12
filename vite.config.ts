188570c52dad37f36ac2ddaa5c99262bfd0c51af
tom9995 <omoituozkanai@yahoo.co.jp>
2024-11-12 23:05:33 +0900

    edit

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/travel-price/",
+  // base: "/travel-price/",
+  base: "./",
   plugins: [react()],
 });
