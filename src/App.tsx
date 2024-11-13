f31a1391d2acc029e22532ac1aae4233eeb63709
tom9995 <omoituozkanai@yahoo.co.jp>
2024-11-13 20:32:06 +0900

    edit

--- src/App.tsx
+++ src/App.tsx
@@ -4,19 +4,22 @@ import Top from "./components/top/Top";
 import ListDetails from "./components/list/listDetails/ListDetails";
 import List from "./components/list/List";
 import Signup from "./components/signup/Signup";
+import { BrowserRouter as Router } from "react-router-dom";
 
 function App() {
   return (
     <>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Top />} />
-          <Route path="/signup" element={<Signup />} />
-          <Route path="/list" element={<List />} />
-          <Route path="/listDetails" element={<ListDetails />} />
-          <Route />
-        </Routes>
-      </BrowserRouter>
+      <Router basename={import.meta.env.DEV ? "/" : "/travel-price"}>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Top />} />
+            <Route path="/signup" element={<Signup />} />
+            <Route path="/list" element={<List />} />
+            <Route path="/listDetails" element={<ListDetails />} />
+            <Route />
+          </Routes>
+        </BrowserRouter>
+      </Router>
     </>
   );
 }
