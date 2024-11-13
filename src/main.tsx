284336411b8c7653b2b91d0020a324900d6e0061
tom9995 <omoituozkanai@yahoo.co.jp>
2024-11-13 20:48:53 +0900

    edit

--- src/main.tsx
+++ src/main.tsx
@@ -4,12 +4,14 @@ import "./index.scss";
 import App from "./App.tsx";
 import { Provider } from "react-redux";
 import { store } from "./app/Store.ts";
+import { BrowserRouter as Router } from "react-router-dom";
 
 createRoot(document.getElementById("root")!).render(
   // <StrictMode>
-
+  // <Router basename={import.meta.env.DEV ? "/" : "/travel-price"}>
   <Provider store={store}>
     <App />
   </Provider>
+  // </Router>
   // </StrictMode>
 );
